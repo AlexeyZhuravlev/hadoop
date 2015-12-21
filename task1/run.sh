c0dad1f545b669dfbf270ff233fe272090608077
AlexeyZhuravlev <aa_Zhur@mail.ru>
2015-12-21 05:36:52 -0800

    rearranged things

--- task1/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input "input" -output "output" -mapper "mapper.py" -reducer "reducer.py" -file /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -file mapper.py -file reducer.py -numReduceTasks 1
