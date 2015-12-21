ffb8caaf38d25d21a2a861c622cb9411410676c7
AlexeyZhuravlev <aa_Zhur@mail.ru>
2015-12-21 13:17:06 +0300

    Pi release

--- pi_number_streaming/mapper.py
+++ pi_number_streaming/mapper.py
@@ -3,13 +3,8 @@
 import sys
 
 for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-    for i in words:
-        q = ''
-        for c in i:
-            if str.isalpha(c):
-                 q = q + c
-        q = str.lower(q)
-        if q != '' and q[0] == 'a':
-            print '%s\t%s' % (q, 1)
+    a, b = list(map(float, line.split()))
+    if a ** 2 + b ** 2 <= 1:
+        print '%s\t%s' % ("I", 1)
+    else:
+        print '%s\t%s' % ("O", 1)
--- pi_number_streaming/reducer.py
+++ pi_number_streaming/reducer.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python2
-"""A more advanced Reducer, using Python iterators and generators."""
 
-from itertools import groupby
-from operator import itemgetter
 import sys
 
-def read_mapper_output(file, separator='\t'):
-    for line in file:
-        yield line.rstrip().split(separator, 1)
+total = 0
+success = 0
 
-def main(separator='\t'):
-    # input comes from STDIN (standard input)
-    data = read_mapper_output(sys.stdin, separator=separator)
-    # groupby groups multiple word-count pairs by word,
-    # and creates an iterator that returns consecutive keys and their group:
-    #   current_word - string containing a word (the key)
-    #   group - iterator yielding all ["&lt;current_word&gt;", "&lt;count&gt;"] items
-    for current_word, group in groupby(data, itemgetter(0)):
-        try:
-            total_count = sum(int(count) for current_word, count in group)
-            print "%s%s%d" % (current_word, separator, total_count)
-        except ValueError:
-            # count was not a number, so silently discard this item
-            pass
+for line in sys.stdin:
+    answer, count = line.split()
+    if answer == "I":
+        success += int(count)
+    total += int(count)
 
-if __name__ == "__main__":
-    main()
+print (float(success) / float(total))
